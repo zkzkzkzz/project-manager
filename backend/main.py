6c020f3072b5e22d57f42ac0e6039c679f43ce6f
zkzkzkzz <zojanikolic00@gmail.com>
2025-04-02 01:28:50 +0200

    intentional ci error for undefined variable

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    long_text = "This is an intentionally very very very very very very very very very very very very very very very very long line of text to trigger a flake8 E501 error."
-    return {"message": "Hello World from Project Manager API!" + long_text}
+    message = 'hello world'
+    result = message + undefined_variable
+    return {'message': result}
 
 
 @app.get("/ping")

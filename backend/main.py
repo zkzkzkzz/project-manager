5736ab2b683abecfe240785ebdd7a9b112bc8694
zkzkzkzz <zojanikolic00@gmail.com>
2025-04-02 01:18:16 +0200

    changed def to async def

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,11 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     long_text = "This is an intentionally very very very very very very very very very very very very very very very very long line of text to trigger a flake8 E501 error."
     return {"message": "Hello World from Project Manager API!" + long_text}
 
 
 @app.get("/ping")
-def get_ping():
+async def get_ping():
     return {"message": "pong"}

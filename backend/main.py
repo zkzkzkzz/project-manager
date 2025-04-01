57e09717d952ff2def87c5d4a1ab8529f5ed5f35
zkzkzkzz <zojanikolic00@gmail.com>
2025-04-02 00:43:56 +0200

    intentional flake8 line length violation

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "fast api working"}
+    long_text = "This is an intentionally very very very very very very very very very very very very very very very very long line of text to trigger a flake8 E501 error."
+    return {"message": "Hello World from Project Manager API!" + long_text}
 
 
 @app.get("/ping")

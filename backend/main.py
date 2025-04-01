79ee77ee2f2af426dac944fecaf528501c76ce00
zkzkzkzz <zojanikolic00@gmail.com>
2025-04-01 04:13:23 +0200

    initial commit of project skeleton

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'message':"fast api working"}
+
+@app.get('/ping')
+def get_ping():
+    return {'message':'pong'}
